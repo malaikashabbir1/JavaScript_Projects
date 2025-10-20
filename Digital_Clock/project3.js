e30831a24a8bffba8ef8ce916807818a8aa01f40
Malaika Shabbir <malaikashabbir1@gmail.com>
2025-10-20 10:46:40 +0500

    Add Guess_A_Number_Project

--- Digital_Clock/project3.js
+++
@@ -1,9 +0,0 @@
-const clock = document.getElementById('clock');
-
-// here 1000 means it will run after every 1 second
-setInterval(function() {
-    let date = new Date();
-    
-    clock.innerHTML = date.toLocaleTimeString();
-
-}, 1000);
